1abda2dc4f41ab3fdbaed394a26c0dd3cf9b96fd
Tim Vaillancourt <tim@timvaillancourt.com>
2017-08-10 22:26:49 +0200

    json hints

--- config/security.go
+++ config/security.go
@@ -16,16 +16,16 @@ package config
 
 // https://docs.mongodb.com/manual/reference/configuration-options/#security-ldap-options
 //type SecurityLdap struct {
-//	Servers	string			`yaml:"servers,omitempty"`
-//	Authz	*SecurityLdapAuthz	`yaml:"authz,omitempty"`
-//	Bind	*SecurityLdapBind	`yaml:"bind,omitempty"`
+//	Servers	string			`yaml:"servers,omitempty", json:"servers,omitempty"`
+//	Authz	*SecurityLdapAuthz	`yaml:"authz,omitempty", json:"authz,omitempty"`
+//	Bind	*SecurityLdapBind	`yaml:"bind,omitempty", json:"bind,omitempty"`
 //}
 
 // https://docs.mongodb.com/manual/reference/configuration-options/#security-options
 type Security struct {
-	Authorization	string		`yaml:"authorization,omitempty"`
-	KeyFile		string		`yaml:"keyFile,omitempty"`
-//	Kmip		*SecurityKmip	`yaml:"kmip,omitempty"`
-//	Ldap		*SecurityLdap	`yaml:"ldap,omitempty"`
-//	Sasl		*SecuritySasl	`yaml:"sasl,omitempty"`
+	Authorization string `yaml:"authorization,omitempty", json:"authorization,omitempty"`
+	KeyFile       string `yaml:"keyFile,omitempty", json:"keyFile,omitempty"`
+	//	Kmip		*SecurityKmip	`yaml:"kmip,omitempty", json:"kmip,omitempty"`
+	//	Ldap		*SecurityLdap	`yaml:"ldap,omitempty", json:"ldap,omitempty"`
+	//	Sasl		*SecuritySasl	`yaml:"sasl,omitempty", json:"sasl,omitempty"`
 }
