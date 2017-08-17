bb6106638df09328723ff2eaaa09ffb04f287bee
Tim Vaillancourt <tim@timvaillancourt.com>
2017-08-18 00:43:21 +0200

    Gofmt fixes

--- config/storage.go
+++ config/storage.go
@@ -2,64 +2,64 @@ package config
 
 // https://docs.mongodb.com/manual/reference/configuration-options/#storage-options
 type Storage struct {
-	DbPath		string			`yaml:"dbPath,omitempty", json:"dbPath,omitempty"`
-	DirectoryPerDB	bool			`yaml:"directorPerDB,omitempty", json:"directorPerDB,omitempty"`
-	Engine		string			`yaml:"engine,omitempty", json:"engine,omitempty"`
-	IndexBuildRetry	bool			`yaml:"indexBuildRetry,omitempty", json:"indexBuildRetry,omitempty"`
-	RepairPath	string			`yaml:"repairPath,omitempty", json:"repairPath,omitempty"`
-	SyncPeriodSecs	int			`yaml:"syncPeriodSecs,omitempty", json:"syncPeriodSecs,omitempty"`
-	Journal		*StorageJournal		`yaml:"journal,omitempty", json:"journal,omitempty"`
-	MMAPv1		*StorageMMAPv1		`yaml:"mmapv1,omitempty", json:"mmapv1,omitempty"`
-	RocksDB		*StorageRocksDB		`yaml:"rocksdb,omitempty", json:"rocksdb,omitempty"`
-	InMemory	*StorageWiredTiger	`yaml:"inMemory,omitempty", json:"inMemory,omitempty"`
-	WiredTiger	*StorageWiredTiger	`yaml:"wiredTiger,omitempty", json:"wiredTiger,omitempty"`
+	DbPath          string             `yaml:"dbPath,omitempty" json:"dbPath,omitempty"`
+	DirectoryPerDB  bool               `yaml:"directorPerDB,omitempty" json:"directorPerDB,omitempty"`
+	Engine          string             `yaml:"engine,omitempty" json:"engine,omitempty"`
+	IndexBuildRetry bool               `yaml:"indexBuildRetry,omitempty" json:"indexBuildRetry,omitempty"`
+	RepairPath      string             `yaml:"repairPath,omitempty" json:"repairPath,omitempty"`
+	SyncPeriodSecs  int                `yaml:"syncPeriodSecs,omitempty" json:"syncPeriodSecs,omitempty"`
+	Journal         *StorageJournal    `yaml:"journal,omitempty" json:"journal,omitempty"`
+	MMAPv1          *StorageMMAPv1     `yaml:"mmapv1,omitempty" json:"mmapv1,omitempty"`
+	RocksDB         *StorageRocksDB    `yaml:"rocksdb,omitempty" json:"rocksdb,omitempty"`
+	InMemory        *StorageWiredTiger `yaml:"inMemory,omitempty" json:"inMemory,omitempty"`
+	WiredTiger      *StorageWiredTiger `yaml:"wiredTiger,omitempty" json:"wiredTiger,omitempty"`
 }
 
 type StorageJournal struct {
-	CommitIntervalMs	int	`yaml:"commitIntervalMs,omitempty", json:"commitIntervalMs,omitempty"`
-	Enabled			bool	`yaml:"enabled,omitempty", json:"enabled,omitempty"`
+	CommitIntervalMs int  `yaml:"commitIntervalMs,omitempty" json:"commitIntervalMs,omitempty"`
+	Enabled          bool `yaml:"enabled,omitempty" json:"enabled,omitempty"`
 }
 
 type StorageRocksDB struct {
-	CacheSizeGB		int	`yaml:"cacheSizeGB,omitempty", json:"cacheSizeGB,omitempty", json:"cacheSizeGB,omitempty"`
-	Compression		string	`yaml:"compression,omitempty", json:"compression,omitempty"`
-	Counters		bool	`yaml:"counters,omitempty", json:"counters,omitempty"`
-	CrashSafeCounters	bool	`yaml:"crashSafeCounters,omitempty", json:"crashSafeCounters,omitempty"`
-	MaxWriteMBPerSec	int	`yaml:"maxWriteMBPerSec,omitempty", json:"maxWriteMBPerSec,omitempty"`
-	SingleDeleteIndex	bool	`yaml:"singleDeleteIndex,omitempty", json:"singleDeleteIndex,omitempty"`
+	CacheSizeGB       int    `yaml:"cacheSizeGB,omitempty" json:"cacheSizeGB,omitempty" json:"cacheSizeGB,omitempty"`
+	Compression       string `yaml:"compression,omitempty" json:"compression,omitempty"`
+	Counters          bool   `yaml:"counters,omitempty" json:"counters,omitempty"`
+	CrashSafeCounters bool   `yaml:"crashSafeCounters,omitempty" json:"crashSafeCounters,omitempty"`
+	MaxWriteMBPerSec  int    `yaml:"maxWriteMBPerSec,omitempty" json:"maxWriteMBPerSec,omitempty"`
+	SingleDeleteIndex bool   `yaml:"singleDeleteIndex,omitempty" json:"singleDeleteIndex,omitempty"`
 }
 
 type StorageMMAPv1Quota struct {
-	Enforced	bool	`yaml:"enforced,omitempty", json:"enforced,omitempty"`
-	MaxFilesPerDB	int	`yaml:"maxFilesPerDB,omitempty", json:"maxFilesPerDB,omitempty"`
+	Enforced      bool `yaml:"enforced,omitempty" json:"enforced,omitempty"`
+	MaxFilesPerDB int  `yaml:"maxFilesPerDB,omitempty" json:"maxFilesPerDB,omitempty"`
 }
 
 // https://docs.mongodb.com/manual/reference/configuration-options/#storage-mmapv1-options
 type StorageMMAPv1 struct {
-	NsSize			int			`yaml:"nsSize,omitempty", json:"nsSize,omitempty"`
-	PreallocDataFiles	bool			`yaml:"preallocDataFiles,omitempty", json:"preallocDataFiles,omitempty"`
-	Quota			*StorageMMAPv1Quota	`yaml:"quota,omitempty", json:"quota,omitempty"`
-	Smallfiles		bool			`yaml:"smallfiles,omitempty", json:"smallfiles,omitempty"`
+	NsSize            int                 `yaml:"nsSize,omitempty" json:"nsSize,omitempty"`
+	PreallocDataFiles bool                `yaml:"preallocDataFiles,omitempty" json:"preallocDataFiles,omitempty"`
+	Quota             *StorageMMAPv1Quota `yaml:"quota,omitempty" json:"quota,omitempty"`
+	Smallfiles        bool                `yaml:"smallfiles,omitempty" json:"smallfiles,omitempty"`
 }
 
 type StorageWiredTigerEngineConfig struct {
-	CacheSizeGB		int	`yaml:"cacheSizeGB,omitempty", json:"cacheSizeGB,omitempty", json:"cacheSizeGB,omitempty"`
-	DirectoryForIndexes	bool	`yaml:"directoryForIndexes,omitempty", json:"directoryForIndexes,omitempty"`
-	JournalCompressor	string	`yaml:"journalCompressor,omitempty", json:"journalCompressor,omitempty"`
-	StatisticsLogDelaySecs	int	`yaml:"statisticsLogDelaySecs,omitempty", json:"statisticsLogDelaySecs,omitempty"`
+	CacheSizeGB            int    `yaml:"cacheSizeGB,omitempty" json:"cacheSizeGB,omitempty" json:"cacheSizeGB,omitempty"`
+	DirectoryForIndexes    bool   `yaml:"directoryForIndexes,omitempty" json:"directoryForIndexes,omitempty"`
+	JournalCompressor      string `yaml:"journalCompressor,omitempty" json:"journalCompressor,omitempty"`
+	StatisticsLogDelaySecs int    `yaml:"statisticsLogDelaySecs,omitempty" json:"statisticsLogDelaySecs,omitempty"`
 }
 
 type StorageWiredTigerCollectionConfig struct {
-	BlockCompression	bool	`yaml:"blockCompression,omitempty", json:"blockCompression,omitempty"`
+	BlockCompression bool `yaml:"blockCompression,omitempty" json:"blockCompression,omitempty"`
 }
 
 type StorageWiredTigerIndexConfig struct {
-	PrefixCompression	bool	`yaml:"prefixCompression,omitempty", json:"prefixCompression,omitempty"`
+	PrefixCompression bool `yaml:"prefixCompression,omitempty" json:"prefixCompression,omitempty"`
 }
 
 // https://docs.mongodb.com/manual/reference/configuration-options/#storage-wiredtiger-options
 type StorageWiredTiger struct {
-	EngineConfig		*StorageWiredTigerEngineConfig		`yaml:"engineConfig,omitempty", json:"engineConfig,omitempty"`
-	CollectionConfig	*StorageWiredTigerCollectionConfig	`yaml:"collectionConfig,omitempty", json:"collectionConfig,omitempty"`
-	IndexConfig		*StorageWiredTigerIndexConfig		`yaml:"indexConfig,omitempty", json:"indexConfig,omitempty"`
+	EngineConfig     *StorageWiredTigerEngineConfig     `yaml:"engineConfig,omitempty" json:"engineConfig,omitempty"`
+	CollectionConfig *StorageWiredTigerCollectionConfig `yaml:"collectionConfig,omitempty" json:"collectionConfig,omitempty"`
+	IndexConfig      *StorageWiredTigerIndexConfig      `yaml:"indexConfig,omitempty" json:"indexConfig,omitempty"`
 }
--- config/system_log.go
+++ config/system_log.go
@@ -1,39 +1,39 @@
 package config
 
 type SystemLogComponent struct {
-	Verbosity	int	`yaml:"verbosity,omitempty", json:"verbosity,omitempty", json:"verbosity,omitempty", json:"verbosity,omitempty"`
+	Verbosity int `yaml:"verbosity,omitempty" json:"verbosity,omitempty" json:"verbosity,omitempty" json:"verbosity,omitempty"`
 }
 
 type SystemLogComponentStorage struct {
-	Journal		*SystemLogComponent	`yaml:"journal,omitempty", json:"journal,omitempty"`
-	Verbosity	int			`yaml:"verbosity,omitempty", json:"verbosity,omitempty", json:"verbosity,omitempty", json:"verbosity,omitempty"`
+	Journal   *SystemLogComponent `yaml:"journal,omitempty" json:"journal,omitempty"`
+	Verbosity int                 `yaml:"verbosity,omitempty" json:"verbosity,omitempty" json:"verbosity,omitempty" json:"verbosity,omitempty"`
 }
 
 // https://docs.mongodb.com/manual/reference/configuration-options/#systemlog-component-options
 type SystemLogComponents struct {
-	AccessControl	*SystemLogComponent		`yaml:"accessControl,omitempty", json:"accessControl,omitempty"`
-	Command		*SystemLogComponent		`yaml:"command,omitempty", json:"command,omitempty"`
-	Control		*SystemLogComponent		`yaml:"control,omitempty", json:"control,omitempty"`
-	Ftdc		*SystemLogComponent		`yaml:"ftdc,omitempty", json:"ftdc,omitempty"`
-	Geo		*SystemLogComponent		`yaml:"geo,omitempty", json:"geo,omitempty"`
-	Index		*SystemLogComponent		`yaml:"index,omitempty", json:"index,omitempty"`
-	Network		*SystemLogComponent		`yaml:"network,omitempty", json:"network,omitempty"`
-	Query		*SystemLogComponent		`yaml:"query,omitempty", json:"query,omitempty"`
-	Replication	*SystemLogComponent		`yaml:"replication,omitempty", json:"replication,omitempty"`
-	Sharding	*SystemLogComponent		`yaml:"sharding,omitempty", json:"sharding,omitempty"`
-	Storage		*SystemLogComponentStorage	`yaml:"storage,omitempty", json:"storage,omitempty"`
-	Write		*SystemLogComponent		`yaml:"write,omitempty", json:"write,omitempty"`
+	AccessControl *SystemLogComponent        `yaml:"accessControl,omitempty" json:"accessControl,omitempty"`
+	Command       *SystemLogComponent        `yaml:"command,omitempty" json:"command,omitempty"`
+	Control       *SystemLogComponent        `yaml:"control,omitempty" json:"control,omitempty"`
+	Ftdc          *SystemLogComponent        `yaml:"ftdc,omitempty" json:"ftdc,omitempty"`
+	Geo           *SystemLogComponent        `yaml:"geo,omitempty" json:"geo,omitempty"`
+	Index         *SystemLogComponent        `yaml:"index,omitempty" json:"index,omitempty"`
+	Network       *SystemLogComponent        `yaml:"network,omitempty" json:"network,omitempty"`
+	Query         *SystemLogComponent        `yaml:"query,omitempty" json:"query,omitempty"`
+	Replication   *SystemLogComponent        `yaml:"replication,omitempty" json:"replication,omitempty"`
+	Sharding      *SystemLogComponent        `yaml:"sharding,omitempty" json:"sharding,omitempty"`
+	Storage       *SystemLogComponentStorage `yaml:"storage,omitempty" json:"storage,omitempty"`
+	Write         *SystemLogComponent        `yaml:"write,omitempty" json:"write,omitempty"`
 }
 
 // https://docs.mongodb.com/manual/reference/configuration-options/#systemlog-options
 type SystemLog struct {
-	Destination		string	`yaml:"destination,omitempty", json:"destination,omitempty"`
-	LogAppend		bool	`yaml:"logAppend,omitempty", json:"logAppend,omitempty"`
-	LogRotate		string	`yaml:"logRotate,omitempty", json:"logRotate,omitempty"`
-	Path			string	`yaml:"path,omitempty", json:"path,omitempty"`
-	Quiet			bool	`yaml:"quiet,omitempty", json:"quiet,omitempty"`
-	TimeStampFormat		string	`yaml:"timeStampFormat,omitempty", json:"timeStampFormat,omitempty"`
-	TraceAllExceptions	bool	`yaml:"traceAllExceptions,omitempty", json:"traceAllExceptions,omitempty"`
-	SyslogFacility		string	`yaml:"syslogFacility,omitempty", json:"syslogFacility,omitempty"`
-	Verbosity		int	`yaml:"verbosity,omitempty", json:"verbosity,omitempty", json:"verbosity,omitempty", json:"verbosity,omitempty"`
+	Destination        string `yaml:"destination,omitempty" json:"destination,omitempty"`
+	LogAppend          bool   `yaml:"logAppend,omitempty" json:"logAppend,omitempty"`
+	LogRotate          string `yaml:"logRotate,omitempty" json:"logRotate,omitempty"`
+	Path               string `yaml:"path,omitempty" json:"path,omitempty"`
+	Quiet              bool   `yaml:"quiet,omitempty" json:"quiet,omitempty"`
+	TimeStampFormat    string `yaml:"timeStampFormat,omitempty" json:"timeStampFormat,omitempty"`
+	TraceAllExceptions bool   `yaml:"traceAllExceptions,omitempty" json:"traceAllExceptions,omitempty"`
+	SyslogFacility     string `yaml:"syslogFacility,omitempty" json:"syslogFacility,omitempty"`
+	Verbosity          int    `yaml:"verbosity,omitempty" json:"verbosity,omitempty" json:"verbosity,omitempty" json:"verbosity,omitempty"`
 }
