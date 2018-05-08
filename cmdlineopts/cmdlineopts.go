404730f05feffb43dc51a5c1b7304dfdf5347394
Tim Vaillancourt <tim@timvaillancourt.com>
2018-05-08 21:38:56 +0200

    First draft of getcmdlineopts #2

---
+++ cmdlineopts/cmdlineopts.go
@@ -0,0 +1,23 @@
+package cmdlineopts
+
+import (
+	"github.com/timvaillancourt/go-mongodb-config/config"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
+
+type CmdLineOpts struct {
+	Argv   []string       `bson:"argv"`
+	Parsed *config.Config `bson:"parsed"`
+	Ok     int            `bson:"ok"`
+}
+
+func (clo *CmdLineOpts) Config() *config.Config {
+	return clo.Parsed
+}
+
+func Get(session *mgo.Session) (*CmdLineOpts, error) {
+	cmdLineOpts := &CmdLineOpts{}
+	err := session.Run(bson.D{{"getCmdLineOpts", "1"}}, cmdLineOpts)
+	return cmdLineOpts, err
+}
