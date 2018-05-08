75f34e8e1c7a3e02d8740635b6c9d253ef5a9e4d
Tim Vaillancourt <tim@timvaillancourt.com>
2018-05-08 22:12:41 +0200

    add code comments

--- cmdlineopts/cmdlineopts.go
+++ cmdlineopts/cmdlineopts.go
@@ -8,22 +8,28 @@ import (
 	"gopkg.in/mgo.v2/bson"
 )
 
+const serverCommand = "getCmdLineOpts"
+
+// CmdLineOpts is a struct for the response of the 'getCmdLineOpts' MongoDB server command
 type CmdLineOpts struct {
 	Argv   []string       `bson:"argv"`
 	Parsed *config.Config `bson:"parsed"`
 	Ok     int            `bson:"ok"`
 }
 
-func (clo *CmdLineOpts) Cmdline() string {
-	return strings.Join(clo.Argv, " ")
+// Cmdline returns a string describing the starting command line of the MongoDB process
+func (opts *CmdLineOpts) Cmdline() string {
+	return strings.Join(opts.Argv, " ")
 }
 
-func (clo *CmdLineOpts) Config() *config.Config {
-	return clo.Parsed
+// Config returns a github.com/timvaillancourt/go-mongodb-config/config.Config struct using the 'getCmdLineOpts' output
+func (opts *CmdLineOpts) Config() *config.Config {
+	return opts.Parsed
 }
 
-func Get(session *mgo.Session) (*CmdLineOpts, error) {
-	cmdLineOpts := &CmdLineOpts{}
-	err := session.Run(bson.D{{"getCmdLineOpts", "1"}}, cmdLineOpts)
-	return cmdLineOpts, err
+// New runs the 'getCmdLineOpts' MongoDB server command on a mgo.Session and returns a pointer to a CmdLineOpts struct and an error
+func New(session *mgo.Session) (*CmdLineOpts, error) {
+	opts := &CmdLineOpts{}
+	err := session.Run(bson.D{{serverCommand, "1"}}, opts)
+	return opts, err
 }
