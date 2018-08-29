5a5cc09294194d910bfc30033fed3a82cb798cf1
Tim Vaillancourt <tim@timvaillancourt.com>
2018-08-29 19:13:01 +0200

    WiredTiger cache size can be a float (#11)

--- config/storage_test.go
+++ config/storage_test.go
@@ -15,7 +15,7 @@ func TestStorage(t *testing.T) {
 	assert.True(t, s.Journal.Enabled, "'storage.journal.enabled' is not true")
 	assert.NotNil(t, s.WiredTiger, "'storage.wiredTiger' is nil/malformed")
 	assert.NotNil(t, s.WiredTiger.EngineConfig, "'storage.wiredTiger' is nil/malformed")
-	assert.Equal(t, 1, s.WiredTiger.EngineConfig.CacheSizeGB, "'storage.engineConfig.cacheSizeGB' is not 1")
+	assert.Equal(t, 1.0, s.WiredTiger.EngineConfig.CacheSizeGB, "'storage.engineConfig.cacheSizeGB' is not 1")
 	assert.NotNil(t, s.RocksDB, "'storage.rocksdb' is nil")
 	assert.True(t, s.RocksDB.Counters, "'storage.rocksdb.counters' is false")
 	assert.NotNil(t, s.MMAPv1, "'storage.mmapv1' is nil")
